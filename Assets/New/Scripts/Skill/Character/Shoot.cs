2f01043f9f01c0a4aaa57f018c36c0290cdcbe7d
김태형 <taehyung@kimtaehyung-ui-MacBook-Pro.local>
2018-11-13 13:55:44 +0900

    Update Assets/New/Scripts/Skill/Character/Shoot.cs
    캐릭터 수명 소모값 수정

--- Assets/New/Scripts/Skill/Character/Shoot.cs
+++ Assets/New/Scripts/Skill/Character/Shoot.cs
@@ -16,7 +16,7 @@ public class Shoot : SkillBase {
 	}
 	public override void FireSkill(CharacterBase character)
 	{
-		// 원거리 공격에 사용되는 수명값은 5
+		// 원거리 공격에 사용되는 수명값은 2
 		// 따라서 5.1f 이상일 때 사용할 수 있도록 함.
 		if (CanFireSkill() && character.CurLife >= 2.1f)
 		{
@@ -27,7 +27,7 @@ public class Shoot : SkillBase {
 			clone.Initialize(character.AttackPerDamage * 0.5f, character.AttackVec);
 
 			// 캐릭터 수명 감소 후 쿨타임 시작
-			character.CurLife -= 5;
+			character.CurLife -= 2;
 			CurTime = 0;
 		}
 	}
